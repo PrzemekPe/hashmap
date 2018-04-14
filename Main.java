3925073767ad498c6e2e747bebfb088cc8a9b141
Przemek <przemyslaw.panas@wp.pl>
2018-04-14 15:30:24 +0200

    weqweqw

---
+++ Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+        map.put(5, "five");
+        map.put(6, "six");
+        map.put(7, "seven");
+
+       // String text = map.get(1);
+       // System.out.println(text);
+
+        for (Map.Entry<Integer, String> entry: map.entrySet()) {
+            int key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println("\nKey " + key + " is for value " + value);
+        }
+
+        }
+    }
+
